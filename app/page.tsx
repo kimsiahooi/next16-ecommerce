0bbfcc712467a1718954ed9e064420d9ae0ca147
Ooi Kim Siah <kimsiahooi@gmail.com>
2025-12-13 18:52:14 +0800

    36

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,29 @@
 import ProductCard from "@/components/product/ProductCard";
 import { prisma } from "@/lib/prisma";
 
-export default async function HomePage() {
-	const products = await prisma.product.findMany();
+type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
 
-	await new Promise((resolve) => setTimeout(resolve, 3000));
+export default async function HomePage(props: { searchParams: SearchParams }) {
+  const searchParams = await props.searchParams;
 
-	return (
-		<main className="container mx-auto p-4">
-			<h1 className="text-3xl font-bold mb-6">Home</h1>
-			<p>Showing {products.length} products</p>
-			<div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-				{products.map((product) => (
-					<ProductCard key={product.id} product={product} />
-				))}
-			</div>
-		</main>
-	);
+  const page = Number(searchParams.page) || 1;
+  const pageSize = 3;
+  const skip = (page - 1) * pageSize;
+
+  const products = await prisma.product.findMany({
+    skip,
+    take: pageSize,
+  });
+
+  return (
+    <main className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-6">Home</h1>
+      <p>Showing {products.length} products</p>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </main>
+  );
 }
