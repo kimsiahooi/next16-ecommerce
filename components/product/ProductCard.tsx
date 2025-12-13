f46d6e15d15aeab4903c2bb236315cdd585b5b17
Ooi Kim Siah <kimsiahooi@gmail.com>
2025-12-13 12:46:58 +0800

    28

--- components/product/ProductCard.tsx
+++ components/product/ProductCard.tsx
@@ -1,19 +1,21 @@
 import Image from "next/image";
-import type { Product } from "@/lib/mocks";
+import type { Product } from "@/app/generated/prisma/client";
 import { formatPrice } from "@/lib/utils";
 
 export default function ProductCard({ product }: { product: Product }) {
   return (
     <div className="border border-gray-200 rounded-lg p-4">
       <div className="relative aspect-video">
-        <Image
-          src={product.image}
-          alt={product.name}
-          className="object-cover"
-          fill
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          loading="eager"
-        />
+        {product.image && (
+          <Image
+            src={product.image}
+            alt={product.name}
+            className="object-cover"
+            fill
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            loading="eager"
+          />
+        )}
       </div>
       <h2 className="text-lg font-semibold"> {product.name}</h2>
       <p className="text-gray-600">{formatPrice(product.price)}</p>
