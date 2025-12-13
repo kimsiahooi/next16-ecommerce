0300e2700c33cfdadebbaae4f124253c13f6ca6b
Ooi Kim Siah <kimsiahooi@gmail.com>
2025-12-13 20:56:26 +0800

    39

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import { Suspense } from "react";
 import ProductCard from "@/components/product/ProductCard";
+import ProductsSkeleton from "@/components/product/ProductsSkeleton";
 import {
   Pagination,
   PaginationContent,
@@ -11,32 +13,49 @@ import { prisma } from "@/lib/prisma";
 
 type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
 
-export default async function HomePage(props: { searchParams: SearchParams }) {
-  const searchParams = await props.searchParams;
-
-  const page = Number(searchParams.page) || 1;
-  const pageSize = 3;
+async function Products({
+  page,
+  pageSize,
+}: {
+  page: number;
+  pageSize: number;
+}) {
   const skip = (page - 1) * pageSize;
 
-  const [products, total] = await Promise.all([
-    prisma.product.findMany({
-      skip,
-      take: pageSize,
-    }),
-    prisma.product.count(),
-  ]);
+  const products = await prisma.product.findMany({
+    skip,
+    take: pageSize,
+  });
 
-  const totalPages = Math.ceil(total / pageSize);
+  await new Promise((resolve) => setTimeout(resolve, 3000));
 
   return (
-    <main className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-6">Home</h1>
+    <>
       <p>Showing {products.length} products</p>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
+    </>
+  );
+}
+
+export default async function HomePage(props: { searchParams: SearchParams }) {
+  const searchParams = await props.searchParams;
+
+  const page = Number(searchParams.page) || 1;
+  const pageSize = 3;
+
+  const total = await prisma.product.count();
+  const totalPages = Math.ceil(total / pageSize);
+
+  return (
+    <main className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-6">Home</h1>
+      <Suspense key={page} fallback={<ProductsSkeleton />}>
+        <Products page={page} pageSize={pageSize} />
+      </Suspense>
 
       <Pagination className="mt-8">
         <PaginationContent>
