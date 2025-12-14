dfbc6634cb471e9cb8f6cf057650813cefffa482
Ooi Kim Siah <kimsiahooi@gmail.com>
2025-12-14 10:54:52 +0800

    43

---
+++ app/products/[slug]/not-found.tsx
@@ -0,0 +1,8 @@
+export default function ProductNotFound() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-6">Product not found</h1>
+      <p>The product you are looking for does not exists.</p>
+    </div>
+  );
+}
