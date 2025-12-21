db2f3097d1ed98ff99158cee6a9f007eef06b085
Ooi Kim Siah <kimsiahooi@gmail.com>
2025-12-21 11:24:15 +0800

    fix: suspense search input

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,54 +1,58 @@
 import { ShoppingCart } from "lucide-react";
 import Link from "next/link";
+import { Suspense } from "react";
 import MobileNav from "./mobile-nav";
 import { ModeToggle } from "./mode-toggle";
 import SearchInput from "./search-input";
 import { Button } from "./ui/button";
 
 export const categories = [
-  { id: 1, name: "Electronics", href: "/categories/electronics" },
-  { id: 2, name: "Fashion", href: "/categories/fashion" },
-  { id: 3, name: "Home", href: "/categories/home" },
+	{ id: 1, name: "Electronics", href: "/categories/electronics" },
+	{ id: 2, name: "Fashion", href: "/categories/fashion" },
+	{ id: 3, name: "Home", href: "/categories/home" },
 ];
 
 export default function Navbar() {
-  return (
-    <div className="border-b">
-      <div className="container mx-auto flex h-16 items-center justify-between">
-        <div>
-          <div className="flex items-center gap-6">
-            <Link className="text-2xl font-bold max-md:hidden" href="/">
-              Store
-            </Link>
-            <nav className="hidden md:flex items-center gap-6">
-              {categories.map((category) => (
-                <Link
-                  key={category.id}
-                  href={category.href}
-                  className="text-sm font-medium text-muted-foreground hover:text-primary transition-colors">
-                  {category.name}
-                </Link>
-              ))}
-            </nav>
-            {/* Mobile Nav */}
-            <MobileNav />
-          </div>
-        </div>
+	return (
+		<div className="border-b">
+			<div className="container mx-auto flex h-16 items-center justify-between">
+				<div>
+					<div className="flex items-center gap-6">
+						<Link className="text-2xl font-bold max-md:hidden" href="/">
+							Store
+						</Link>
+						<nav className="hidden md:flex items-center gap-6">
+							{categories.map((category) => (
+								<Link
+									key={category.id}
+									href={category.href}
+									className="text-sm font-medium text-muted-foreground hover:text-primary transition-colors"
+								>
+									{category.name}
+								</Link>
+							))}
+						</nav>
+						{/* Mobile Nav */}
+						<MobileNav />
+					</div>
+				</div>
 
-        <div className="w-full mx-4 md:mx-8 max-md:hidden">
-          <SearchInput />
-        </div>
+				<div className="w-full mx-4 md:mx-8 max-md:hidden">
+					<Suspense>
+						<SearchInput />
+					</Suspense>
+				</div>
 
-        <div className="flex items-center gap-4">
-          <Button variant="ghost" size="icon" asChild>
-            <Link href="/cart">
-              <ShoppingCart className="size-5" />
-            </Link>
-          </Button>
+				<div className="flex items-center gap-4">
+					<Button variant="ghost" size="icon" asChild>
+						<Link href="/cart">
+							<ShoppingCart className="size-5" />
+						</Link>
+					</Button>
 
-          <ModeToggle />
-        </div>
-      </div>
-    </div>
-  );
+					<ModeToggle />
+				</div>
+			</div>
+		</div>
+	);
 }
